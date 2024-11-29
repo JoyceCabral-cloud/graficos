8816d6164912de96da2a37c3da338419dc4f8876
JoyceCabral-cloud <joycemayara.cabral@gmail.com>
2024-11-29 10:49:01 +0000

    Create matplotlib3.py
    frutas

---
+++ matplotlib3.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+#Exemplo de um gráfico circular
+
+frutas = ['Bananas', 'Maçãs', 'Pêras', 'Morangos']
+quantidades_vendidas = [40, 55, 25, 50]
+
+plt.pie(quantidades_vendidas, labels=frutas, autopct='%1.2f%%')
+plt.title('Gráfico Circular')
+plt.show()
