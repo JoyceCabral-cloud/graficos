9a96a7d7f5dabecbc5ee86204e628be4258a0997
JoyceCabral-cloud <joycemayara.cabral@gmail.com>
2024-11-29 10:51:59 +0000

    Update matplotlib3.py
    commit uvas

--- matplotlib3.py
+++ matplotlib3.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 
 #Exemplo de um gráfico circular
 
-frutas = ['Bananas', 'Maçãs', 'Pêras', 'Morangos']
-quantidades_vendidas = [40, 55, 25, 50]
+frutas = ['Bananas', 'Maçãs', 'Pêras', 'Morangos', 'Uvas']
+quantidades_vendidas = [40, 55, 25, 50, 68]
 
 plt.pie(quantidades_vendidas, labels=frutas, autopct='%1.2f%%')
 plt.title('Gráfico Circular')
